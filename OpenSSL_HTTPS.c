85a53b0ec26054f909d48ef4220facf19061c8e9
Min Peace <128281240+Peace-Min@users.noreply.github.com>
2023-05-11 21:36:53 +0900

    Add files via upload
    소켓 생성을 기존에 소켓 함수를 활용하지 않고 OpenSSL의 BIO를 사용

--- OpenSSL_HTTPS.c
+++ OpenSSL_HTTPS.c
@@ -14,12 +14,8 @@
 #define CERT_FILE "cert.pem"
 #define KEY_FILE "key.pem"
 #define MAX 1024
-SSL_CTX *ctx;
-void * serverThread(void *arg);
-void setupServerCtx(void);
-void bind_and_listen(int serv_sock, struct sockaddr_in* serv_adr, int backlog, int port);
-void accept_connection(int serv_sock, struct sockaddr_in clnt_addr);
-int serverLoop(SSL * ssl);
+
+SSL_CTX* setupServerCtx(void);
 void * serverThread(void *arg);
 void* handle_client_request(SSL* ssl);
 void send_data_ssl(SSL* ssl, char* ct, char* file_name);
@@ -28,63 +24,59 @@ void send_error(SSL* ssl);
 void error_handling(char* message);
 
 int main(int argc,char *argv[]) {
-	int serv_sock;
-	struct sockaddr_in serv_addr, clnt_addr;
+	SSL_CTX *ctx;
 	SSL *ssl;
-	X509 *cert;
+	pthread_t tid;
+	BIO *acc,*client;
+	char *port;
+	//SSL_CTX SET
+	ctx=setupServerCtx();
+	//PORT NUM SET
+	port=(char *)malloc(sizeof(char)*10);
+	strcpy(port,argv[1]);
+	//BIO set port
+	if (!(acc = BIO_new_accept(port)))
+		error_handling("BIO_new_accept() error");
+	//first do_accept > socket create, bind
+	if (BIO_do_accept(acc) <= 0)
+		error_handling("1st BIO_do_accept() error");
 
-	setupServerCtx();
-
-	//socket
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	bind_and_listen(serv_sock, &serv_addr, 20, atoi(argv[1]));
-
-	while (1) {
-		accept_connection(serv_sock, clnt_addr);
+	for (;;) {
+		//second do_accept > wait client
+		if (BIO_do_accept(acc) <= 0)
+			error_handling("2nd BIO_do_accept() error");
+		//latest client pop
+		client = BIO_pop(acc);
+		if (!(ssl = SSL_new(ctx)))
+			error_handling("SSL_new() error");
+		//client input/output set
+		SSL_set_bio(ssl, client, client);
+		pthread_create (&tid, NULL, serverThread, ssl);
 	}
 	SSL_CTX_free(ctx);
+	BIO_free(acc);
 }
-void setupServerCtx(void){
+SSL_CTX* setupServerCtx(void){
+	SSL_CTX *ctx;
     //SSL library memset-> 0
 	SSL_library_init();
-	const SSL_METHOD *method;
-	method = TLS_server_method();
-	ctx = SSL_CTX_new(method);
+	
+	ctx = SSL_CTX_new(TLS_server_method());
+
 	if(SSL_CTX_use_certificate_file(ctx, CERT_FILE, SSL_FILETYPE_PEM) != 1)
 		error_handling("cert err");
+
 	if(SSL_CTX_use_PrivateKey_file(ctx, KEY_FILE, SSL_FILETYPE_PEM)	!= 1)
 		error_handling("private err");
-}
-void bind_and_listen(int serv_sock, struct sockaddr_in* serv_adr, int backlog, int port){
-	memset(serv_adr, 0, sizeof(struct sockaddr_in));
-	serv_adr->sin_family = AF_INET;
-	serv_adr->sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr->sin_port = htons(port);
-
-	if(	(bind(serv_sock, (struct sockaddr*)serv_adr, sizeof(struct sockaddr_in))) == -1)
-		error_handling("bind err");
-	if(	(listen(serv_sock, backlog)) == -1)
-		error_handling("listen err");
-}
-void accept_connection(int serv_sock, struct sockaddr_in clnt_addr){
-	SSL *ssl;
-	int clnt_sock;
-	pthread_t tid;
-	socklen_t clnt_addr_size;
-	clnt_addr_size = sizeof(clnt_addr);
-	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, (socklen_t*)&clnt_addr_size);
-
-	ssl = SSL_new(ctx);
-	SSL_set_fd(ssl,clnt_sock);
-	pthread_create (&tid, NULL, serverThread, ssl);
-	pthread_detach(tid);
+	return ctx;
 }
 void * serverThread(void *arg){
 	SSL *ssl = (SSL *) arg;
 
+	pthread_detach(pthread_self());
+	//Server-Client Hand-Shake
 	if (SSL_accept(ssl) <= 0){
-		error_handling("hand-shake err");
-		SSL_free(ssl);
+		error_handling("Hand-Shake err");
 		return 0;
 	}
 	fprintf(stderr, "SSL Connection opened\n");
@@ -97,9 +89,11 @@ void* handle_client_request(SSL *ssl) {
 	char ct[MAX];
 	char file_name[MAX];
 	char buf[MAX];
+	// treats an unexpected EOF from the peer as if normally shutdown
+	SSL_set_options(ssl, SSL_OP_IGNORE_UNEXPECTED_EOF);
 
 	SSL_read(ssl, req_line, MAX);	
-    /*HTTP 헤더가 아닌경우 종료*/
+    //not HTTP header close
 	if (strstr(req_line, "HTTP/")== NULL) {
 		send_error(ssl);
 		SSL_shutdown(ssl);
@@ -108,7 +102,7 @@ void* handle_client_request(SSL *ssl) {
 	}
 	strcpy(method, strtok(req_line, " /"));
 
-    /*GET 메소드가 아닌경우 종료*/
+    //not GET method close
 	if (strcmp(method, "GET") != 0) {
 		send_error(ssl);
 		SSL_shutdown(ssl);
@@ -116,14 +110,14 @@ void* handle_client_request(SSL *ssl) {
 		return 0;
 	}
 
-    /*파일 전송*/
+    //file communication
 	strcpy(file_name, strtok(NULL, " /"));
 	strcpy(ct, content_type(file_name));
-	send_data_ssl(ssl, ct, file_name); //입력받은 데이터에 해당하는 파일을 전송함
-
+	//data type check
+	send_data_ssl(ssl, ct, file_name);
 }
 
-/*http 헤더를 받고 지정된 파일을 전송한다.*/
+//data transmit
 void send_data_ssl(SSL* ssl, char* ct, char* file_name) {
 	char protocol[] = "HTTP/1.0 200 OK\r\n";
 	char server[] = "Server:Linux Web Server\r\n";
@@ -154,8 +148,7 @@ void send_data_ssl(SSL* ssl, char* ct, char* file_name) {
 	SSL_shutdown(ssl);
 	SSL_free(ssl);
 }
-
-/*헤더를 읽어와서 헤더의 파일요청 방식이 html이면 html 반환하고 아니면 text 반환*/
+//file header check-> html or plain
 char* content_type(char* file) {
 	char extension[MAX];
 	char file_name[MAX];
@@ -174,8 +167,7 @@ void send_error(SSL* ssl) {
 	char server[] = "Server:Linux Web Server\r\n";
 	char cnt_len[] = "Content-length:2048\r\n";
 	char cnt_type[] = "Content-type:text/html\r\n\r\n";
-	char content[] = "<html><head><title>NETWORK</title><head>""<body><font size+=10><br>오류 발생 요청 파일명 및 요청 방식 확인""</font></body></html>";
-
+	char content[] = "<html><head><title>NETWORK</title><head>""<body><font size+=10><br>error occur""</font></body></html>";
 	SSL_write(ssl, protocol, strlen(protocol));
 	SSL_write(ssl, server, strlen(server));
 	SSL_write(ssl, cnt_len, strlen(cnt_len));
@@ -185,5 +177,5 @@ void send_error(SSL* ssl) {
 void error_handling(char* message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
-	return;
+	return ;
 }
